49261f57e5a05d1fea018e98f7fb55eceefd1a9d
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2014-02-19 20:10:43 -0800

    re-organization

--- Router.php
+++ Router.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Route - standalone routing engine to dispatch to your controller class methods
+Route - standalone routing engine that dispatched to callables
 Licensed under MIT. See LICENSE.txt for details.
 https://github.com/alanszlosek/tiny-helpers
 
@@ -8,16 +8,15 @@ EXAMPLE
 
 URL: http://abc.com/category/123/?offset=2
 
-Make your routes data structure:
+Make your routes data structure. Routes() accepts parameters in pairs. The first matches a URL folder path, the second is the configuration for that folder.
 
 	$routes = Routes(
-		'category',
-			Routes(
-				// :integer is an internal alias that matches integers
-				// $path array will be passed to OrderController constructor
-				// byId() will be called with an object that looks like this JSON: {id:123}
-				':integer' => Route::To('OrderController', 'byId', '//id'),
-			)
+		'category', Routes(
+			// :integer is an internal alias that matches integers
+			// $path array will be passed to OrderController constructor
+			// byId() will be called with an object that looks like this JSON: {id:123}
+			':integer', Route::toClassMethod('OrderController', 'byId')->label('id')
+		)
 	);
 
 Dispatch by passing the path portion of your URL to dispatch, without the scheme, domain or query string.
@@ -26,26 +25,54 @@ Ensure no repeat slashes (/categories//something///) before calling dispatch().
 	$path = "/category/123/";
 	// dispatch() returns whatever your controller method returns
 	echo $routes->dispatch($path);
-
-EXTEND FOR MORE POWER
-
-If you extend the Router class you can even use your route tree to generate navigation for your site.
-
-Extend RouteLeaf if you want to route to something else besides methods or callables.
 */
 
-function Routes() {
-	$args = func_get_args();
-	$r = new Router($args);
-	return $r;
+// YOU SHOULD IMPLEMENT THIS FUNCTION YOURSELF, SINCE YOU MAY WANT TO EXTEND THE Router CLASS
+if (!function_exists('Routes')) {
+	function Routes() {
+		$args = func_get_args();
+		$r = new Router($args);
+		return $r;
+	}
 }
 
-class Router {
-	// $runnable is intended to be an instance of a RouteLeaf class, with a run() method
-	public $runnable;
-	public $label;
-	public $routes = array();
+// Functionality common to the Router and Route classes
+// Meant to be the base class for route tree leaves. They do the handoff from Router to the rest of your application
+class RouteNode {
+	/*
+	$callable can be:
+
+	array('ClassName', 'methodName')
+	array($classInstance, 'methodName')
+	'functionName'
+	*/
+	protected $callable;
+	protected $label;
 	public $parent;
+
+	public function __construct($callable) {
+		$this->callable = $callable;
+	}
+
+	public function hasLabel() {
+		return isset($this->label);
+	}
+
+	public function label($label = null) {
+		if ($label == null) return $this->label;
+		$this->label = $label;
+		return $this;
+	}
+
+	// Instantiate and run
+	public function run($labels) {
+		$callable = $this->callable;
+		return $callable($labels);
+	}
+}
+
+class Router extends RouteNode {
+	public $routes = array();
 
 	public function __construct($routes) {
 		for ($i = 0; $i < sizeof($routes); $i+=2) {
@@ -55,12 +82,6 @@ class Router {
 			$this->routes[$key] = $route;
 		}
 	}
-	
-	public function label($label) {
-		$this->label = $label;
-		return $this;
-	}
-
 
 	/**
 	 * This method is called recursively.
@@ -68,18 +89,21 @@ class Router {
 	 */
 	public function dispatch($path, $labels = null) {
 		// First run
-		if ($labels == null) {
+		if (!is_array($path)) {
 			$labels = new stdClass;
+			$labels->__routerInstance = $this;
 			$path = trim($path, '/');
+			$labels->__pathString = $path;
 			// Don't explode an empty string ... it does weird things
 			$path = ($path ? explode('/', $path) : array());
+			$labels->__pathArray = $path;
 		}
 
 		// No more path to digest
 		if (!$path) {
 			// If we have no runnable, it'll return false (404)
-			if ($this->runnable instanceof RouteLeaf) {
-				return $this->runnable->run($labels);
+			if ($this->callable) {
+				return $this->run($labels);
 			} else {
 				return false;
 			}
@@ -87,23 +111,25 @@ class Router {
 
 		$part = array_shift($path);
 
-		// If the current route level has been given a label, use it to label the current path portion
-		if ($this->label) {
-			$label = $this->label;
-			$labels->$label = $part;
-		}
-
+		// Feel liek this can be merged with the !$path check above
 		$route = $this->getRoute($part);
 		if (!$route) {
 			// No route found, 404 time
 			return false;
 		}
 
+		// If the current route level has been given a label, use it to label the current path portion
+		if ($route->hasLabel()) {
+			$label = $route->label();
+			$labels->$label = $part;
+		}
+
 		// If there is no more path to digest, this next call to dispatch will trigger the runnable
-		if ($route instanceof RouteLeaf) {
-			return $route->run($labels);
-		} else {
+		if ($route instanceof Router) {
 			return $route->dispatch($path, $labels);
+		} else {
+			// If it's not an instance of Router, it SHOULD be an instance of RouteCallable
+			return $route->run($labels);
 		}
 	}
 
@@ -121,50 +147,18 @@ class Router {
 
 
 	public function toClassMethod($class, $method) {
-		$this->runnable = Route::toClassMethod($class, $method);
+		$this->callable(array($class, $method));
 		return $this;
 	}
 }
 
 
-// The following 3 classes could use some more elegance
 class Route {
 	public static function toClassMethod($class, $method) {
-		return new RouteLeafClassMethod($class, $method);
+		return new RouteNode(array($class, $method));
 	}
 	public static function toCallable($callable) {
-		return new RouteLeaf($callable);
-	}
-}
-
-class RouteLeaf {
-	public $parent;
-	protected $callable;
-
-	public function __construct($callable) {
-		$this->callable = $callable;
-	}
-
-	// Instantiate and run
-	public function run($labels) {
-		$callable = $this->callable;
-		return $callable($labels);
-	}
-}
-
-class RouteLeafClassMethod extends RouteLeaf {
-	public function __construct($class, $method) {
-		parent::__construct(array($class, $method));
-	}
-
-	// Instantiate and run
-	public function run($labels) {
-		$class = $this->callable[0];
-		$method = $this->callable[1];
-		if (!is_object($class)) { // If we were given a class name, instead of an instance
-			$c = new $class(null);
-		}
-		return $c->$method($labels);
+		return new RouteNode($callable);
 	}
 }
 
