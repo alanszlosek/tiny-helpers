92cc5c6c2f8c49b3008c06ed371c7992edf22a89
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2014-05-26 19:26:53 -0400

    re-arrange

--- Loader.php
+++ Loader.php
@@ -10,47 +10,99 @@ So my loader is less specific about each class, and more about setting a string 
 
 */
 class Loader {
-    // Can set a default base directory, to make config less tedious
-    public static $base = '';
-    // This is the PSR-4 bit
-    public static $namespacePrefixes = array();
+    // Base path for PSR-0 auto-loading .. . without a trailing directory separator
+    public static $basePSR0 = '';
+    // Tree for PSR-4 auto-loading
+    public static $treePSR4 = '';
+
     // This is the normal include path bit
     public static $paths = array();
 
+    // After attempting PSR-4 then PSR-0, we try normal include paths
     public static function loadClass($className) {
         $nl = "\n";
-        // 
 
+        $eh = static::loadPSR4($className);
+        if ($eh) {
+            return $eh;
+        }
+
+        $eh = static::loadPSR0($className);
+        if ($eh) {
+            return $eh;
+        }
+
+        return false;
+    }
+
+    // PSR-4
+    public static function loadPSR4($className) {
+        if (!isset(static::$treePSR4)) {
+            return false;
+        }
+        $tree = static::$treePSR4;
+        $parts = explode('\\', trim($className, '\\'));
+
+        $classFile = str_replace('_', DIRECTORY_SEPARATOR, array_pop($parts));
+        $paths = array();
+        $left = array();
+        while ($part = array_shift($parts)) {
+            if (!isset($tree[$part])) {
+                // No namespace map
+                break;
+            }
+            $paths = array_merge($paths, $tree[$part]['__paths']);
+            $left[] = $part;
+        }
+
+        $fileName = implode(DIRECTORY_SEPARATOR, $parts) . $classFile . '.php';
+        // Try to find that class file in these base directories
+        foreach ($paths as $path) {
+            $full = $path . DIRECTORY_SEPARATOR . $fileName;
+            //echo $full . $nl;
+            if (file_exists($full)) {
+                require($full);
+                return $full;
+            }
+        }
+        return false;
+   }
+
+    public static function addNamespace($prefix, $path) {
+        $parts = explode('\\', trim($prefix, '\\'));
+        $tree = &static::$treePSR4;
+
+        foreach ($parts as $part) {
+            if (!isset($tree[ $part ])) {
+                $tree[ $part ] = array(
+                    '__paths' => array()
+                );
+            }
+            $tree = &$tree[ $part ];
+        }
+        $tree['__paths'][] = $path;
+    }
+
+
+    // PSR-0
+    public static function loadPSR0($className) {
         // From the PSR-0 spec
         $className = ltrim($className, '\\');
-        $fileName  = '';
+        $fileName  = static::$basePSR0 . DIRECTORY_SEPARATOR;
         $namespace = '';
         if ($lastNsPos = strrpos($className, '\\')) {
             $namespace = substr($className, 0, $lastNsPos);
             $className = substr($className, $lastNsPos + 1);
-            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+            $fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
         }
         $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-        $sub = '/' . $fileName;
-
-        // Try to find that class file in these base directories
-        foreach (static::$paths as $path) {
-            if ($path[0] != DIRECTORY_SEPARATOR) {
-                $full = static::$base . DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR . $fileName;
-            } else {
-                $full = $path . DIRECTORY_SEPARATOR . $fileName;
-            }
-            //echo $full . $nl;
-            if (file_exists($full)) {
-                require($full);
-                break;
-            }
+        if (file_exists($fileName)) {
+            require($fileName);
+            return true;
         }
-        $full = static::$base . DIRECTORY_SEPARATOR . $fileName;
-        if (file_exists($full)) {
-            require($full);
-        }
+        return false;
     }
+
 }
 
--- tests/LoaderTests.php
+++ tests/LoaderTests.php
@@ -4,6 +4,47 @@ require_once('../Loader.php');
 
 class RouteTests extends PHPUnit_Framework_TestCase {
 
+    // Test PSR-0 including
+	public function testPSR0() {
+        Loader::$basePSR0 = __DIR__ . DIRECTORY_SEPARATOR . 'Loader';
+        spl_autoload_register(array('Loader', 'loadClass'));
+
+        $classes = array(
+            'Aardvark' => 'ants', // root namespace
+            '\\Aardvark\\Boardwalk' => 'water',
+            '\\Aardvark\\Boardwalk\\Cat' => 'scratch',
+        );
+
+        foreach ($classes as $class => $ret) {
+            $a = new $class();
+            $this->assertEquals($ret, $a->ident());
+        }
+    }
+
+    // Test PSR-4 including
+	public function testPSR4() {
+        Loader::addNamespace('Prefixed', __DIR__ . DIRECTORY_SEPARATOR . 'Loader/Aardvark');
+        Loader::addNamespace('GeneratedNamespace', __DIR__ . DIRECTORY_SEPARATOR . 'Loader' . DIRECTORY_SEPARATOR . 'generated/GeneratedNamespace');
+        spl_autoload_register(array('Loader', 'loadClass'));
+
+        $classes = array(
+            // Namespace is part of class name, but not part of file system
+            'Aardvark\\Boardwalk' => 'water',
+            'Aardvark\\Boardwalk\\Cat' => 'scratch',
+            'GeneratedNamespace\\GenClass' => 'manufactured',
+        );
+
+        foreach ($classes as $class => $ret) {
+            $a = new $class();
+            $this->assertEquals($ret, $a->ident());
+        }
+    }
+
+
+
+
+/*
+    // Test PHP include paths
 	public function testSimple() {
         Loader::$base = __DIR__ . DIRECTORY_SEPARATOR . 'Loader';
         Loader::$paths = array(
@@ -25,4 +66,5 @@ class RouteTests extends PHPUnit_Framework_TestCase {
             $this->assertEquals($ret, $a->ident());
         }
     }
+*/
 }
