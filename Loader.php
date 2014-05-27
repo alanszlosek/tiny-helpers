a64754cea24c76aba516c993c6a7d7b6138b4d9a
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2014-05-26 20:36:28 -0400

    less static

--- Loader.php
+++ Loader.php
@@ -11,23 +11,20 @@ So my loader is less specific about each class, and more about setting a string 
 */
 class Loader {
     // Base path for PSR-0 auto-loading .. . without a trailing directory separator
-    public static $basePSR0 = '';
+    public $basePSR0 = '';
     // Tree for PSR-4 auto-loading
-    public static $treePSR4 = '';
-
-    // This is the normal include path bit
-    public static $paths = array();
+    protected $treePSR4 = '';
 
     // After attempting PSR-4 then PSR-0, we try normal include paths
-    public static function loadClass($className) {
+    public function loadClass($className) {
         $nl = "\n";
 
-        $eh = static::loadPSR4($className);
+        $eh = $this->loadPSR4($className);
         if ($eh) {
             return $eh;
         }
 
-        $eh = static::loadPSR0($className);
+        $eh = $this->loadPSR0($className);
         if ($eh) {
             return $eh;
         }
@@ -36,41 +33,39 @@ class Loader {
     }
 
     // PSR-4
-    public static function loadPSR4($className) {
-        if (!isset(static::$treePSR4)) {
+    protected function loadPSR4($className) {
+        if (!isset($this->treePSR4)) {
             return false;
         }
-        $tree = static::$treePSR4;
+        $tree = $this->treePSR4;
         $parts = explode('\\', trim($className, '\\'));
 
         $classFile = str_replace('_', DIRECTORY_SEPARATOR, array_pop($parts));
         $paths = array();
-        $left = array();
         while ($part = array_shift($parts)) {
             if (!isset($tree[$part])) {
                 // No namespace map
                 break;
             }
             $paths = array_merge($paths, $tree[$part]['__paths']);
-            $left[] = $part;
+            $tree = $tree[$part];
         }
 
         $fileName = implode(DIRECTORY_SEPARATOR, $parts) . $classFile . '.php';
         // Try to find that class file in these base directories
-        foreach ($paths as $path) {
+        foreach (array_reverse($paths) as $path) {
             $full = $path . DIRECTORY_SEPARATOR . $fileName;
             //echo $full . $nl;
-            if (file_exists($full)) {
-                require($full);
+            if ($this->requireFile($full)) {
                 return $full;
             }
         }
         return false;
    }
 
-    public static function addNamespace($prefix, $path) {
+    public function addNamespace($prefix, $path) {
         $parts = explode('\\', trim($prefix, '\\'));
-        $tree = &static::$treePSR4;
+        $tree = &$this->treePSR4;
 
         foreach ($parts as $part) {
             if (!isset($tree[ $part ])) {
@@ -85,10 +80,10 @@ class Loader {
 
 
     // PSR-0
-    public static function loadPSR0($className) {
+    protected function loadPSR0($className) {
         // From the PSR-0 spec
         $className = ltrim($className, '\\');
-        $fileName  = static::$basePSR0 . DIRECTORY_SEPARATOR;
+        $fileName  = $this->basePSR0 . DIRECTORY_SEPARATOR;
         $namespace = '';
         if ($lastNsPos = strrpos($className, '\\')) {
             $namespace = substr($className, 0, $lastNsPos);
@@ -99,6 +94,14 @@ class Loader {
 
         if (file_exists($fileName)) {
             require($fileName);
+            return true;
+        }
+        return false;
+    }
+
+    protected function requireFile($file) {
+        if (file_exists($file)) {
+            require($file);
             return true;
         }
         return false;
--- tests/LoaderTests.php
+++ tests/LoaderTests.php
@@ -2,12 +2,21 @@
 
 require_once('../Loader.php');
 
+class MockLoader extends Loader {
+    public $files = array();
+
+    protected function requireFile($file) {
+        return in_array($file, $this->files);
+    }
+}
+
 class RouteTests extends PHPUnit_Framework_TestCase {
 
     // Test PSR-0 including
 	public function testPSR0() {
-        Loader::$basePSR0 = __DIR__ . DIRECTORY_SEPARATOR . 'Loader';
-        spl_autoload_register(array('Loader', 'loadClass'));
+        $loader = new Loader();
+        $loader->basePSR0 = __DIR__ . DIRECTORY_SEPARATOR . 'Loader';
+        spl_autoload_register(array($loader, 'loadClass'));
 
         $classes = array(
             'Aardvark' => 'ants', // root namespace
@@ -23,9 +32,10 @@ class RouteTests extends PHPUnit_Framework_TestCase {
 
     // Test PSR-4 including
 	public function testPSR4() {
-        Loader::addNamespace('Prefixed', __DIR__ . DIRECTORY_SEPARATOR . 'Loader/Aardvark');
-        Loader::addNamespace('GeneratedNamespace', __DIR__ . DIRECTORY_SEPARATOR . 'Loader' . DIRECTORY_SEPARATOR . 'generated/GeneratedNamespace');
-        spl_autoload_register(array('Loader', 'loadClass'));
+        $loader = new Loader();
+        $loader->addNamespace('Prefixed', __DIR__ . DIRECTORY_SEPARATOR . 'Loader/Aardvark');
+        $loader->addNamespace('GeneratedNamespace', __DIR__ . DIRECTORY_SEPARATOR . 'Loader' . DIRECTORY_SEPARATOR . 'generated/GeneratedNamespace');
+        spl_autoload_register(array($loader, 'loadClass'));
 
         $classes = array(
             // Namespace is part of class name, but not part of file system
@@ -40,7 +50,70 @@ class RouteTests extends PHPUnit_Framework_TestCase {
         }
     }
 
+    public function testPSR4two() {
+        $loader = new MockLoader();
 
+        $loader->files = array(
+            '/vendor/foo.bar/src/ClassName.php',
+            '/vendor/foo.bar/src/DoomClassName.php',
+            '/vendor/foo.bar/tests/ClassNameTest.php',
+            '/vendor/foo.bardoom/src/ClassName.php',
+            '/vendor/foo.bar.baz.dib/src/ClassName.php',
+            '/vendor/foo.bar.baz.dib.zim.gir/src/ClassName.php',
+        );
+
+        $loader->addNamespace(
+            'Foo\Bar',
+            '/vendor/foo.bar/src'
+        );
+
+        $loader->addNamespace(
+            'Foo\Bar',
+            '/vendor/foo.bar/tests'
+        );
+
+        $loader->addNamespace(
+            'Foo\BarDoom',
+            '/vendor/foo.bardoom/src'
+        );
+
+        $loader->addNamespace(
+            'Foo\Bar\Baz\Dib',
+            '/vendor/foo.bar.baz.dib/src'
+        );
+
+        $loader->addNamespace(
+            'Foo\Bar\Baz\Dib\Zim\Gir',
+            '/vendor/foo.bar.baz.dib.zim.gir/src'
+        );
+
+        // public function testExistingFile()
+        $actual = $loader->loadClass('Foo\Bar\ClassName');
+        $expect = '/vendor/foo.bar/src/ClassName.php';
+        $this->assertSame($expect, $actual);
+
+        $actual = $loader->loadClass('Foo\Bar\ClassNameTest');
+        $expect = '/vendor/foo.bar/tests/ClassNameTest.php';
+        $this->assertSame($expect, $actual);
+
+        // public function testMissingFile()
+        $actual = $loader->loadClass('No_Vendor\No_Package\NoClass');
+        $this->assertFalse($actual);
+
+        // public function testDeepFile()
+        $actual = $loader->loadClass('Foo\Bar\Baz\Dib\Zim\Gir\ClassName');
+        $expect = '/vendor/foo.bar.baz.dib.zim.gir/src/ClassName.php';
+        $this->assertSame($expect, $actual);
+
+        // public function testConfusion()
+        $actual = $loader->loadClass('Foo\Bar\DoomClassName');
+        $expect = '/vendor/foo.bar/src/DoomClassName.php';
+        $this->assertSame($expect, $actual);
+
+        $actual = $loader->loadClass('Foo\BarDoom\ClassName');
+        $expect = '/vendor/foo.bardoom/src/ClassName.php';
+        $this->assertSame($expect, $actual);
+    }
 
 
 /*
